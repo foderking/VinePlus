e614ab4dc5dbd121278b5e05ab46975455b2a03e
foderking <freatshow@gmail.com>
2023-04-13 12:40:10 +0100

    fixed bugs with threadPost parser and added tests

--- Comicvine.Core.Test/ParserTest.fs
+++ Comicvine.Core.Test/ParserTest.fs
@@ -2,7 +2,6 @@ module Tests.Parser
 
 open System
 open System.Net.Http
-open System.Threading.Tasks
 open Comicvine.Core
 open Xunit
 
@@ -21,12 +20,29 @@ let getNodeFromPath path = task {
     return Main.getRootNode stream
 }
 
-let testShouldNotThrow parser path = task {
+let inline testShouldNotThrow parser path = task {
     let! node = getNodeFromPath path
-    // let z = parser node
-    let ex = Record.Exception(tryTask parser node)
+    
+    let ex = Record.Exception(fun () ->
+        parser node
+        |> ignore
+    )
     Assert.Null(ex)
 }
+
+module Helpers =
+    [<Fact>]
+    let ``function raising exception should fail "testShouldNotThrow" helper`` () =
+        let f _ =
+            raise (Exception("bla"))
+        // let ex = Record.Exception(
+        //     testShouldNotThrow f ""
+        // )
+        Assert.ThrowsAsync<Xunit.Sdk.NullException>(
+            fun () ->
+                testShouldNotThrow f ""
+        )
+    
 
 module BlogParser =
     [<Fact>]
@@ -41,13 +57,137 @@ module BlogParser =
         
 
 module CommentParser =
-    [<Fact>]
-    let ``parsing valid thread with comments should not fail``() =
-        testShouldNotThrow (Parsers.ParseComments 2300312) "/forums/gen-discussion-1/new-mcu-captain-marvel-statement-2300312/"
+    // [<Fact>]
+    // let ``parsing valid thread with comments should not fail``() = task {
+    //     let! node = getNodeFromPath "/forums/gen-discussion-1/new-mcu-captain-marvel-statement-2300312/"
+    //     // let ex = Record.Exception(
+    //     //     fun () ->
+    //             // testShouldNotThrow (Parsers.ParseComments 2300312) "/forums/gen-discussion-1/new-mcu-captain-marvel-statement-2300312/"
+    //             // raise (Exception("adssfafd"))
+    //             //
+    //     Parsers.ParseComments 2300312 node
+    //     |> Seq.map( fun e ->
+    //         match e with
+    //         | ThreadPost.OP(z) -> z.Created
+    //         | ThreadPost.Comment(z) -> z.Created
+    //     )
+    //     |> printf "%A"
+    //     // )
+    //     // printf "%A" (Parsers.ParseComments 2300312 node)
+    //     // Assert.Null(ex)
+    //     Assert.True(true)
+    // }
 
     [<Fact>]
     let ``valid comments should not be empty``() = task {
         let! node = getNodeFromPath "/forums/gen-discussion-1/new-mcu-captain-marvel-statement-2300312/"
         let j = Parsers.ParseComments 2300312 node
-        Assert.NotEmpty (j)
+        Assert.NotEmpty j
     }
+    
+    [<Fact>]
+    let ``should correctly show edited posts exists``() = task {
+        let! node = getNodeFromPath "/forums/gen-discussion-1/new-mcu-captain-marvel-statement-2300312/"
+        let j = Parsers.ParseComments 2300312 node
+                |> Seq.map (fun e ->
+                    match e with
+                    | Parsers.ThreadPost.OP(n) -> n.IsEdited
+                    | Parsers.ThreadPost.Comment(n) -> n.IsEdited
+                )
+        Assert.False(j |> Seq.isEmpty )
+    }
+    
+    [<Fact>]
+    let ``date created for each post should be unique``() = task {
+        let! node = getNodeFromPath "/forums/gen-discussion-1/new-mcu-captain-marvel-statement-2300312/"
+        let j = Parsers.ParseComments 2300312 node
+        let k = j
+                |> Seq.map (fun e ->
+                    match e with
+                    | Parsers.ThreadPost.OP(n) -> n.Created
+                    | Parsers.ThreadPost.Comment(n) -> n.Created
+                )
+                |> Seq.distinct
+                
+        Assert.True((j |> Seq.length) = (k |> Seq.length ))
+    }   
+    
+    [<Fact>]
+    let ``"ThreadId" value should be correct and the same``() = task {
+        let! node = getNodeFromPath "/forums/gen-discussion-1/new-mcu-captain-marvel-statement-2300312/"
+        let j = Parsers.ParseComments 2300312 node
+        let k = j
+                |> Seq.map (fun e ->
+                    match e with
+                    | Parsers.ThreadPost.OP(n) -> n.ThreadId
+                    | Parsers.ThreadPost.Comment(n) -> n.ThreadId
+                )
+                |> Seq.filter (fun x -> x = 2300312)
+                
+        Assert.True((j |> Seq.length) = (k |> Seq.length ))
+    }   
+    
+    [<Fact>]
+    let ``content of each post should not be empty``() = task {
+        let! node = getNodeFromPath "/forums/gen-discussion-1/new-mcu-captain-marvel-statement-2300312/"
+        let j = Parsers.ParseComments 2300312 node
+        let k = j
+                |> Seq.map (fun e ->
+                    match e with
+                    | Parsers.ThreadPost.OP(n) -> n.Content
+                    | Parsers.ThreadPost.Comment(n) -> n.Content
+                )
+                |> Seq.filter (fun x -> x.Length > 0)
+                
+        Assert.True((j |> Seq.length) = (k |> Seq.length ))
+    }
+    
+    [<Fact>]
+    let ``creator details should not be empty``() = task {
+        let! node = getNodeFromPath "/forums/gen-discussion-1/new-mcu-captain-marvel-statement-2300312/"
+        let j = Parsers.ParseComments 2300312 node
+        let k = j
+                |> Seq.map (fun e ->
+                    match e with
+                    | Parsers.ThreadPost.OP(n) -> n.Creator
+                    | Parsers.ThreadPost.Comment(n) -> n.Creator
+                )
+                |> Seq.filter (fun x -> x.Link.Length > 0 && x.Text.Length > 0)
+                
+        Assert.True((j |> Seq.length) = (k |> Seq.length ))
+    }
+    
+    [<Fact>]
+    let ``comment ids should be distinct``() = task {
+        let! node = getNodeFromPath "/forums/gen-discussion-1/new-mcu-captain-marvel-statement-2300312/"
+        let j = Parsers.ParseComments 2300312 node
+                |> Seq.map (fun e ->
+                    match e with
+                    | Parsers.ThreadPost.OP _ -> None
+                    | Parsers.ThreadPost.Comment(n) -> Some(n.Id)
+                )
+                |> Seq.filter Option.isSome
+                |> Seq.map Option.get
+        let k = j
+                |> Seq.distinct
+                
+        Assert.True((j |> Seq.length) = (k |> Seq.length ))
+    }
+    
+    [<Fact>]
+    let ``"PostNo" should be distinct``() = task {
+        let! node = getNodeFromPath "/forums/gen-discussion-1/new-mcu-captain-marvel-statement-2300312/"
+        let j = Parsers.ParseComments 2300312 node
+                |> Seq.map (fun e ->
+                    match e with
+                    | Parsers.ThreadPost.OP _ -> None
+                    | Parsers.ThreadPost.Comment(n) -> Some(n.PostNo)
+                )
+                |> Seq.filter Option.isSome
+                |> Seq.map Option.get
+        let k = j
+                |> Seq.distinct
+                
+        Assert.True((j |> Seq.length) = (k |> Seq.length ))
+    }
+    
