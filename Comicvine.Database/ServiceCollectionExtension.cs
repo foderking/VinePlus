e5e960348e4cbce93781a5ae2145095d939a5b9e
foderking <foderking@gmail.com>
2023-05-16 17:31:59 +0100

    added ef design reference to web project, and updated workflow

--- Comicvine.Database/ServiceCollectionExtension.cs
+++ Comicvine.Database/ServiceCollectionExtension.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Comicvine.Database;
 
 // https://medium.com/swlh/creating-a-multi-project-net-core-database-solution-a69decdf8d7e
+// when running migrations, web project should be referenced as startup project
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection RegisterDataServices(this IServiceCollection services, IConfiguration configuration) {
