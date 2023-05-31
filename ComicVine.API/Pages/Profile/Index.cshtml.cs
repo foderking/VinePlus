e2666bf148063e68b68a0c71e71f7f8f4a25e5c7
foderking <foderking@gmail.com>
2023-05-31 19:56:43 +0100

    added pages for profile

---
+++ ComicVine.API/Pages/Profile/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Comicvine.Core;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ComicVine.API.Pages.Profile;
+
+public class Index : PageModel
+{
+    private Parsers.ISingle<Parsers.Profile> parser;
+    public string User_ = "";
+    
+    public Index(Parsers.ISingle<Parsers.Profile> p) {
+        parser = p;
+    }
+    public async Task OnGet(string user) {
+        var profile = await Parsers.Common.ParseDefault(parser, $"/profile/{user}");
+        User_ = profile.UserName;
+    }
+}
