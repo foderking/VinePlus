54aac48a22865a05cf39b991b14578c700e426a8
foderking <freatshow@gmail.com>
2023-06-06 17:16:09 +0100

    added view for profile

--- ComicVine.API/Pages/Profile/Index.cshtml.cs
+++ ComicVine.API/Pages/Profile/Index.cshtml.cs
@@ -5,10 +5,14 @@ namespace ComicVine.API.Pages.Profile;
 
 public class Index : PageModel
 {
+    public Parsers.Profile? UserProfile;
     public string UserName = "";
+
+    public string FallBackImage =
+        "https://comicvine.gamespot.com/a/uploads/original/11173/111735759/8970504-6560831187-Blank.jpg";
     
     public async Task OnGet(string user) {
-        var profile = await Parsers.ProfileParser.ParseDefault($"/profile/{user}");
-        UserName = profile.UserName;
+        UserProfile = await Parsers.ProfileParser.ParseDefault($"/profile/{user}");
+        UserName = UserProfile.UserName;
     }
 }
