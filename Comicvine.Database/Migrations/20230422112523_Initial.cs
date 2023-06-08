8aaa387aa50d1928a7aaa689ea683a701d8973e8
foderking <foderking@gmail.com>
2023-06-08 09:12:33 +0100

    committed migrations

---
+++ Comicvine.Database/Migrations/20230422112523_Initial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Comicvine.Core;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Comicvine.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "threads",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    thread = table.Column<Parsers.Link>(type: "jsonb", nullable: true),
+                    board = table.Column<Parsers.Link>(type: "jsonb", nullable: true),
+                    is_pinned = table.Column<bool>(type: "boolean", nullable: false),
+                    is_locked = table.Column<bool>(type: "boolean", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    type = table.Column<int>(type: "integer", nullable: false),
+                    last_post_no = table.Column<int>(type: "integer", nullable: false),
+                    last_post_page = table.Column<int>(type: "integer", nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    total_posts = table.Column<int>(type: "integer", nullable: false),
+                    total_view = table.Column<int>(type: "integer", nullable: false),
+                    creator = table.Column<Parsers.Link>(type: "jsonb", nullable: true),
+                    comments = table.Column<int[]>(type: "integer[]", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_threads", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "threads");
+        }
+    }
+}
