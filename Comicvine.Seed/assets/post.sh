bee7bd61a7ed1a3f29f80064202c6e6379110600
foderking <foderking@gmail.com>
2023-06-10 00:40:36 +0100

    more refactors

--- Comicvine.Seed/assets/post.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/sh
-
-cat $1.json | \
-    jq -rn --stream 'fromstream(1 | truncate_stream(inputs)) | select(.) | [.Id,.IsComment,.IsDeleted,.IsModComment,.PostNo,(.Creator|tojson),.IsEdited,.Created,.Content,.ThreadId] | @csv' > $1.csv
--- Comicvine.Seed/assets/thread.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/sh
-cat $1.json |  jq -r '(.[] | [.Id,(.Thread | tojson),(.Board | tojson),.IsPinned,.IsLocked,.IsDeleted,.Type,.LastPostNo,.LastPostPage,.Created,.TotalPosts,.TotalView,(.Creator|tojson)]) | @csv' > $1.csv
