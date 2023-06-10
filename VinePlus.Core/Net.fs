363efc20911ff1443bcc0fde33b9e623459becc9
foderking <foderking@gmail.com>
2023-06-10 15:27:15 +0100

    renamed the core library

--- VinePlus.Core/Net.fs
+++ VinePlus.Core/Net.fs
@@ -16,48 +16,26 @@ module Net =
       return! client.GetStringAsync($"{path}?{q}")
     }
   
-  // let getReaderWithHandler(query: Dictionary<string, string>)(handler: HttpClientHandler)(path: string) = task {
-  //     use querystring = new FormUrlEncodedContent(query)
-  //     let! q = querystring.ReadAsStringAsync()
-  //     let client = new HttpClient(handler, true)
-  //     client.BaseAddress <- Uri("https://comicvine.gamespot.com")
-  //     return! client.GetStringAsync($"{path}?{q}")
-  //   }
-    
-      // use querystring = new FormUrlEncodedContent(query)
-      // let! q = querystring.ReadAsStringAsync()
-      // let client = new HttpClient()
-      // client.BaseAddress <- Uri("https://comicvine.gamespot.com")
-      // return! client.GetStreamAsync($"{path}?{q}")
-    // }
   let getUrlWithCancellation (ct: CancellationToken) (query: Dictionary<string, string>) (path: string) = task {
       use querystring = new FormUrlEncodedContent(query)
       let! q = querystring.ReadAsStringAsync(ct)
       let client = new HttpClient()
       client.BaseAddress <- Uri("https://comicvine.gamespot.com")
       return! client.GetStringAsync($"{path}?{q}")
-    // return! client.GetStreamAsync("/forums/battles-7/kotor-sith-lords-vs-cw-sith-lords-1631389/?page=2");
   }
 
-
-
+  /// creates an html node from an html string
   let createRootNode (html: string) =
     let rootNode = HtmlDocument()
     rootNode.LoadHtml(html)
     rootNode.DocumentNode
     
+  /// convert a specific paginated page into an html node
   let getNodeFromPage(path: string)(page: int) = 
     getStringFromUrl (Dictionary[KeyValuePair("page", page |> string)]) path
     |> Task.map createRootNode
     
+  /// converts the page at a path to an html node
   let getNodeFromPath(path: string) = 
     getStringFromUrl (Dictionary<string,string>()) path
     |> Task.map createRootNode
-
-  // let map (mapper: 'T -> 'U) (task_: Task<'T>) = task {
-  //     let! t = task_
-  //     return mapper t
-  // }
-  // let mapSeq (mapper: 'T -> 'U) (s: Task<'T> seq)  =
-  //     s
-  //     |> Seq.map (map mapper)
