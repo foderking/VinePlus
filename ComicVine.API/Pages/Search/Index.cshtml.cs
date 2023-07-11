86245088801bc9101412c38fba6c9a67fc4d7de6
foderking <foderking@gmail.com>
2023-07-11 15:51:03 +0100

    basic search working

--- ComicVine.API/Pages/Search/Index.cshtml.cs
+++ ComicVine.API/Pages/Search/Index.cshtml.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ComicVine.API.Pages.Search;
 
+[IgnoreAntiforgeryToken(Order = 1001)] // disables anti-forgery token requirement   https://www.learnrazorpages.com/security/request-verification
 public class Index : PageModel
 {
+    private ILogger<Index> _logger;
+    public Index(ILogger<Index> _log) {
+        _logger = _log;
+    }
     public void OnGet() {
+        _logger.LogInformation("get");
+    }
+
+     public IActionResult OnPost(string? creator, string searchQuery) {
+        return Redirect($"/search/results?searchPost=false&query={searchQuery}");
     }
 }
