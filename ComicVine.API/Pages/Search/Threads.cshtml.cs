a79e8dfdacb141a129c8114fb08fdf85e0c50b84
foderking <freatshow@gmail.com>
2023-07-11 16:53:10 +0100

    added UI for thread search

---
+++ ComicVine.API/Pages/Search/Threads.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ComicVine.API.Pages.Search;
+
+[IgnoreAntiforgeryToken(Order = 1001)] // disables anti-forgery token requirement   https://www.learnrazorpages.com/security/request-verification
+public class SearchThreads : PageModel
+{
+    public void OnGet() {
+    }
+        
+     public IActionResult OnPost(string? creator, string searchQuery) {
+        return Redirect($"/search/results?searchPost=false&query={searchQuery}");
+    }
+}
