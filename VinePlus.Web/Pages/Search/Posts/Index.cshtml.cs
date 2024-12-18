5df2ec61ce5dae36642bed65e692a6dc898fd6f9
foderking <foderking@gmail.com>
2024-12-18 11:46:30 +0100

    fixed bug where post request wasn't working

--- VinePlus.Web/Pages/Search/Posts/Index.cshtml.cs
+++ VinePlus.Web/Pages/Search/Posts/Index.cshtml.cs
@@ -3,13 +3,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ComicVine.API.Pages.Search.Posts;
 
-[IgnoreAntiforgeryToken(Order = 1002)]
+[IgnoreAntiforgeryToken(Order = 1001)]
 public class Index : PageModel
 {
     public void OnGet() {
         
     }
     public IActionResult OnPost(string creator, string searchQuery) {
-        return Redirect($"/search/results?searchPost=true&query={searchQuery.Trim()}&creator={creator.Trim()}");
+        return Redirect($"/search/posts/results?searchPost=true&query={searchQuery.Trim()}&creator={creator.Trim()}");
     }
 }
