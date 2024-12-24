b327f7a385f9e596dd392b3a49d6a9aaaa6cdef5
foderking <foderking@gmail.com>
2024-12-24 15:45:25 +0100

    added stat for top threads

--- VinePlus.Web/Pages/Stats/Index.cshtml.cs
+++ VinePlus.Web/Pages/Stats/Index.cshtml.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using VinePlus.Database;
 
 namespace VinePlus.Web.Pages.Stats;
 
-public class Index : PageModel
+public class Index(ComicvineContext context) : PageModel
 {
+    public IEnumerable<ThreadView> top10_threads;
     public void OnGet() {
+        top10_threads = Queries.getArchivedThreads(context, 1, SortForumBy.NoPosts).Take(10);
     }
 }
